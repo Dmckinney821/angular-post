bcad0dea391155fef6c067f5fbb4757e40ca745e
Dan McKinney <3dhdaniel@gmail.com>
2019-09-12 13:54:50 -0400

    thirsty thursday

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+
+import {
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialog,
+    MatDialogModule,
+   
+  } from "@angular/material";
+  
+
+
+@NgModule({
+    exports: [
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatExpansionModule,
+        MatProgressSpinnerModule,
+        MatPaginatorModule,
+        MatDialogModule
+    ]
+})
+
+export class AngularMaterialModule {
+
+}
